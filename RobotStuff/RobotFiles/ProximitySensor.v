9a0d7df53658f11007b2372f8fab81f596686a62
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-02-28 17:49:52 -0600

    fixed proximity sensor

--- RobotStuff/RobotFiles/ProximitySensor.v
+++ RobotStuff/RobotFiles/ProximitySensor.v
@@ -2,12 +2,16 @@
 module ProximitySensor(output reg trigger, input echo, input clk, output reg isCrash); //trigger begins ranging, echo gives ranging value, clk is used for timing
     //need to supply a short 10us pulse to trigger ranging                          is crash is ouput to motor module
     reg [16:0]timer = 0; //timer used for trigger output (minimum 10 microseconds)
-    reg distance; //actual distance value used for determining if something is in range. Interpreted from distanceTimer
+    reg [31:0]distance; //actual distance value used for determining if something is in range. Interpreted from distanceTimer
     reg distanceTimer = 0; //Distance is measured in time it takes for the sound pulse to get back to the sensor
     //basys3 board FPGA has a 450 MHz clock 450,000,000 clks/s = 450/us
     // 10us = 4500 clocks
-    reg [16:0]tenMicro = 4500>>1; //~10us
-    initial begin
+    reg [16:0]tenMicro = 1000; //~10us
+    reg [16:0]frequencyStepperValue = 100000;
+    reg [16:0]stepTimer;
+    reg steppedClock = 0;
+    stepTimer = frequencyStepperValue;
+        initial begin
         trigger = 0;
     end
     always @(posedge clk) //every clock pulse
@@ -22,20 +26,32 @@ module ProximitySensor(output reg trigger, input echo, input clk, output reg isC
                   timer = timer - 1;
                 end
         end
-    always @(posedge echo) //when echo goes high begin updating distanceTimer
+    
+    always @(posedge clk)
         begin
-          if(echo) //only when echo is high update (this avoids a always@(negedge echo) statement)
-            begin
-                distanceTimer = distanceTimer + 1;
-                distance <=(distanceTimer*34000)>>1;
-            end
+            if(stepTimer <= frequencyStepperValue)
+                begin
+                stepTimer = timer - 1;
+                end
+            if(stepTimer == 0)
+                begin
+                steppedClock = ~steppedClock;
+                end
         end
   
+    always@(posedge steppedClock)
+        begin
+            if(echo)
+                begin
+                distanceTimer = distanceTimer+1;
+                distance = distanceTimer*340;
+                end
+        end
     //speed of sound (m/s) * time (s) = distance (m)   time must be halved because pulse goes to and back from location
 
   always@(negedge echo)
     begin
-    if(distance >= 6 && distance <= 15) //within 6 to 15 cms or 3 to 6 inches
+    if(distance >= 153 && distance <= 306) //within 6 to 15 cms or 3 to 6 inches
         begin
           isCrash = 1; //you're gonna crash dawg
         end
