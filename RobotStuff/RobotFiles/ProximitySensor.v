d04bff9121c0f4c82222ca86d10eb7830a6c5df2
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-02-28 20:33:33 -0600

    removed bottom range of sensor, moved distance timer calculation to always @negedge echo

--- RobotStuff/RobotFiles/ProximitySensor.v
+++ RobotStuff/RobotFiles/ProximitySensor.v
@@ -4,16 +4,12 @@ module ProximitySensor(output reg trigger, input echo, input clk, output reg isC
     reg [16:0]timer = 0; //timer used for trigger output (minimum 10 microseconds)
     reg [31:0]distance; //actual distance value used for determining if something is in range. Interpreted from distanceTimer
     reg distanceTimer = 0; //Distance is measured in time it takes for the sound pulse to get back to the sensor
-    //basys3 board FPGA has a 450 MHz clock 450,000,000 clks/s = 450/us
-    // 10us = 4500 clocks
     reg [16:0]tenMicro = 1000; //~10us
     reg [16:0]frequencyStepperValue = 100000;
     reg [16:0]stepTimer;
     reg steppedClock = 0;
-        initial begin
-        trigger = 0;
-        stepTimer = frequencyStepperValue;
-    end
+    trigger = 0;
+    stepTimer = frequencyStepperValue;
     always @(posedge clk) //every clock pulse
         begin
             if(timer == 0) //if the timer is 0 invert trigger (turn on at start and off at end)
@@ -26,7 +22,6 @@ module ProximitySensor(output reg trigger, input echo, input clk, output reg isC
                   timer = timer - 1;
                 end
         end
-    
     always @(posedge clk)
         begin
             if(stepTimer <= frequencyStepperValue)
@@ -38,26 +33,26 @@ module ProximitySensor(output reg trigger, input echo, input clk, output reg isC
                 steppedClock = ~steppedClock;
                 end
         end
-  
     always@(posedge steppedClock)
         begin
             if(echo)
                 begin
-                distanceTimer = distanceTimer+1;
-                distance = distanceTimer*340;
+                (distanceTimer = distanceTimer+1);
                 end
         end
     //speed of sound (m/s) * time (s) = distance (m)   time must be halved because pulse goes to and back from location
-
   always@(negedge echo)
     begin
-    if(distance >= 153 && distance <= 306) //within 6 to 15 cms or 3 to 6 inches
+    distance = (distanceTimer*340)>>1;
+    if(distance <= 306) //within 6 inches
         begin
-          isCrash = 1; //you're gonna crash dawg
+          isCrash = 1; //you're gonna crash
         end
     else
         begin
           isCrash = 0; //all is good
         end
-        end
+    end
 endmodule
+ //basys3 board FPGA has a 100 MHz clock 100,000,000 clks/s = 1/10ns
+    // 10us = 1000 clocks
