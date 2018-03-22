a583b3d2ad584351dd111747b9d09830608f3f2c
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-03-22 10:08:10 -0500

    changed top file to be used with proximity sensor module

--- RobotStuff/RobotFiles/Top.v
+++ RobotStuff/RobotFiles/Top.v
@@ -1,23 +1,18 @@
-module Top(input [2:0] JB, input [5:0]JA);
+module Top(output [1:0]JA, input clk, input [1:0]JB);
             //uncomment for motor
             // sw[2] sw[1] sw[0] =  inductance 3 bit number set to switches for test
             // JA[3] JA[2] JA[1] JA[0] = IN4 IN3 IN2 IN1    
             // JB[1] JB[0] = ENA ENB
-            wire [2:0]induct;
-            assign induct[2:0] = JB[2:0];
-            reg proxim = 0;
-            reg red = 0;
-            wire [3:0]MotorIn;
-            wire [1:0]MotorEn;
-            assign MotorIn = JA[3:0];
-            assign MotorEn = JA[5:4];
-            Motor motorTest(induct, proxim, MotorIn, MotorEn, red);
-            // wire trigger;
-            // wire outLED;
-            // assign trigger = JA[1];
-            // assign outLED = JA[2];
-            // assign echo = JB[0];
-            // ProximitySensor prox(trigger, echo, clk, outLED);
+//            reg red = 0;
+//            reg proxim = 0;
+//            wire [2:0]induct;
+//            assign induct[2:0] = JB[2:0];
+//            wire [3:0]MotorIn;
+//            wire [1:0]MotorEn;
+//            assign MotorIn = JA[3:0];
+//            assign MotorEn = JA[5:4];
+//            Motor motorTest(induct, proxim, MotorIn, MotorEn, red);
+             ProximitySensor prox(JA[0], JB[0], clk, JA[1]);
             
             
             
