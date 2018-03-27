dce074aaf1ee7e726a7b5d9f9f654949368eabfb
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-03-27 09:20:13 -0500

    removed accidental uncomment

--- RobotStuff/RobotFiles/ProximitySensor.v
+++ RobotStuff/RobotFiles/ProximitySensor.v
@@ -1,4 +1,4 @@
-The rover contains four motors, two of which are removed for this project, that are rated at 7.2 Volts and a max speed of .62 miles per hour`timescale 1us/1us
+`timescale 1us/1us
 module ProximitySensor(output reg trigger, input echo, input clk, output reg isCrash); //trigger begins ranging, echo gives ranging value, clk is used for timing
     //need to supply a short 10us pulse to trigger ranging                          is crash is ouput to motor module
  //timer used for trigger output (minimum 10 microseconds)
