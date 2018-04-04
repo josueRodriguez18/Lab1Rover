4fdfc5c911aa41e36b1a5a912d2ab6314d2c9164
Dong Hyuk Shin <35859492+Anthony-Shin@users.noreply.github.com>
2018-04-04 16:48:59 -0500

    Add files via upload
    prototype

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,10 +1,33 @@
-module ColorSensor(clk, data, dataOut);
-    parameter width;
-    input clk, [0:7]data;
-    output [0:width]dataOut;
-    reg[0:7]colorValue; //8-bit RGB color value from 0-255
+module ColorSensor(output [1:0] scale, output [1:0]filter, output enf, output [2:0] color, input Freq, input clk)
+
+always@(posedge clk)
     
+    begin
+      scale <= 2b'11;
+      enf <= 1;
+      Freq = Frequncy();
+      //if(counter increment)
+        filter <= 2b'01;
 
+        if( x < Freq && Freq < y)
+            begin
+                color = 3'b001;        
+            end
 
+        filter <= 2b'10;
+
+        if(x < Freq && Freq < y)
+            begin
+                color = 3'b010;  
+            end
+            
+
+        filter <= 2b'11;
+
+        if (x < Freq && Freq < y)
+            begin
+                color = 3'b100;
+            end
+    end    
 
 endmodule
