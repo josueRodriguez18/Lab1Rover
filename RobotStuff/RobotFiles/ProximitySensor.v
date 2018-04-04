f37188b6bce7a246ade4d5dc659003e1b25d4c07
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-04-04 13:43:19 -0500

    Proximity sensor now only takes one sample every 30 ms, should be ready to implement

--- RobotStuff/RobotFiles/ProximitySensor.v
+++ RobotStuff/RobotFiles/ProximitySensor.v
@@ -2,34 +2,50 @@
 module ProximitySensor(output reg trigger, input echo, input clk, output reg isCrash); //trigger begins ranging, echo gives ranging value, clk is used for timing
     //need to supply a short 10us pulse to trigger ranging                          is crash is ouput to motor module
  //timer used for trigger output (minimum 10 microseconds)
-    reg [31:0]distance = 0;
-    
-    always @(posedge clk) //every clock pulse
-        begin
-            case (echo)
-            0:
+  reg [31:0] counter = 32'b0;
+  reg [31:0] distance = 100000;
+  reg [31:0] echo_cntr = 32'b0;
+  
+  always@(posedge clk)
+    begin
+        if(counter <= 1000)
+            begin
+              trigger = 1'b1;
+            end
+        else
+            begin
+              trigger = 1'b0;
+            end
+        if(echo)
+            begin
+              echo_cntr <= echo_cntr + 1;
+            end
+        else
+            begin
+              if(counter_echo)
                 begin
-                    
-                    if(distance <= 294117 && distance > 0)
+                    if(counter_echo < distance)
                         begin
-                            isCrash <= 1;
-                            distance <=0;
+                          isCrash = 1'b1;
                         end
-                     else if(distance > 294117)
+                    else
                         begin
-                            isCrash <= 0;
-                            distance <= 0;
+                          isCrash = 0;
                         end
-                        trigger = 1;
-                    
-                end       
-            1:
+                end
+            end
+
+            if(counter == 3000000)
                 begin
                     trigger <= 0;
-                    distance <= distance + 1;
+                    counter <= 0;
+                    counter _echo <= 1'b0;
                 end
-                endcase
-        end
+            else
+                begin
+                  counter <= counter +1;
+                end
+    end
     
     //speed of sound (m/s) * time (s) = distance (m)   time must be halved because pulse goes to and back from location
 
