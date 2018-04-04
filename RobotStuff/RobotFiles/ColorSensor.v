86c5ce4a086d65a47fd6bfd91a4c3ae27ce65a08
Dong Hyuk Shin <35859492+Anthony-Shin@users.noreply.github.com>
2018-04-04 18:14:30 -0500

    Color sensor should be good now

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,33 +1,58 @@
-module ColorSensor(output [1:0] scale, output [1:0]filter, output enf, output [2:0] color, input Freq, input clk)
+module ColorSensor(output reg [1:0] scale, output reg [1:0]filter, output reg enf, output reg [2:0] color, input sensorFreq, input clk)
+reg [31:0] Freq;
+reg [31:0] gate = 1000000;
+reg [31:0] count = 0;
+reg [31:0] tempFreq = 0;
+parameter x;
+parameter y;
 
 always@(posedge clk)
-    
+	begin
+		scale <= 2b'11;
+		enf <= 1'b1;
+		if(count != gate)
+			begin
+				count = count + 1;
+			end
+		else
+			begin
+				count = 0;
+				Freq= tempFreq;
+			end
+		if(sensorFreq)
+			begin
+				tempFreq = tempFreq +1;
+			end
+	end
+
+always@(negedge count)
     begin
-      scale <= 2b'11;
-      enf <= 1;
-      Freq = Frequncy();
-      //if(counter increment)
-        filter <= 2b'01;
-
-        if( x < Freq && Freq < y)
-            begin
-                color = 3'b001;        
+	  case(filter)
+		00: //RED
+			begin
+				if(x <= Freq && Freq <= y)
+					begin
+					color = 3'b001;        
+					end
+				filter = 2'b01;
+			end
+		01: //BLUE
+			begin
+				if(x <= Freq && Freq <= y)
+					begin
+						color = 3'b010;  
+					end
+				filter = 2'b10;
+			end
+		11: //GREEN
+			begin
+				if (x <= Freq && Freq <= y)
+					begin
+						color = 3'b100;
+					end
+				filter = 2'b00;
             end
-
-        filter <= 2b'10;
-
-        if(x < Freq && Freq < y)
-            begin
-                color = 3'b010;  
-            end
-            
-
-        filter <= 2b'11;
-
-        if (x < Freq && Freq < y)
-            begin
-                color = 3'b100;
-            end
-    end    
+		endcase
+    end
 
 endmodule
