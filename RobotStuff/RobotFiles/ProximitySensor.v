22cf4bfa8b1e23274a9a045d40b3973728770c83
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-04-04 16:45:27 -0500

    proximity sensors

--- RobotStuff/RobotFiles/ProximitySensor.v
+++ RobotStuff/RobotFiles/ProximitySensor.v
@@ -1,11 +1,8 @@
 `timescale 1us/1us
 module ProximitySensor(output reg trigger, input echo, input clk, output reg isCrash); //trigger begins ranging, echo gives ranging value, clk is used for timing
-    //need to supply a short 10us pulse to trigger ranging                          is crash is ouput to motor module
- //timer used for trigger output (minimum 10 microseconds)
   reg [31:0] counter = 32'b0;
   reg [31:0] distance = 100000;
   reg [31:0] echo_cntr = 32'b0;
-  
   always@(posedge clk)
     begin
         if(counter <= 1000)
