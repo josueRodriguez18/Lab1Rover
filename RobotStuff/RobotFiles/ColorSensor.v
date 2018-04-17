15279fe509b967a73fda8a168d13df890843af86
Dong Hyuk Shin <35859492+Anthony-Shin@users.noreply.github.com>
2018-04-17 15:32:05 -0500

    Updated

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,59 +1,59 @@
 module ColorSensor(output reg [1:0] scale, output reg [1:0]filter, output reg enf, output reg [2:0] color, input sensorFreq, input clk);
 reg [31:0] Freq; //kHz
-reg [31:0] gate = 1000000; //1 ms
-reg [31:0] count = 0;
 reg [31:0] tempFreq = 0;
-reg [31:0] avRedFreq; reg[31:0]avRedFeq; reg[31:0] avBlueFreq;
-reg [31:0] tolerance;
+reg [31:0] RedFreq; 
+reg [31:0]BlueFreq; 
+reg [31:0] GreenFreq;
+reg enf = 0;
 
-
-always@(posedge clk)
-	begin
-		scale <= 2'b11;
-		enf <= 1'b1;
-		if(count <= gate)
-			begin
-				count = count + 1;
-			end
-		else
-			begin
-				count = 0;
-				Freq= tempFreq;
-			end
-		if(sensorFreq)
-			begin
-				tempFreq = tempFreq +1;
-			end
-	end
-
-always@(count==0)
+always@(!Freq)
     begin
-	  case(filter)
-		00: //RED
-			begin
-				if(avRedFreq - tolerance <= Freq && Freq <= avRedFreq + tolerance)
-					begin
-					color = 3'b001;        
+			
+			FreqCounter f1(clk, sensorFreq, Freq);
+	  	
+			case(filter)
+				2'b00: //RED
+					begin				
+				
+						RedFreq = Freq;
+						filter = 2'b01;
+						Freq = 0;
+
 					end
-				filter = 2'b01;
-			end
-		01: //BLUE
-			begin
-				if(avBlueFreq - tolerance <= Freq && Freq <= avBlueFreq + tolerance)
+				2'b01: //BLUE
 					begin
-						color = 3'b010;  
+				
+						BlueFreq = Freq;
+						filter = 2'b11;
+						Freq = 0;
+				
 					end
-				filter = 2'b10;
-			end
-		11: //GREEN
-			begin
-				if (avGreenFreq - tolerance <= Freq && Freq <= avGreenFreq + tolerance)
+				2'b11: //GREEN
 					begin
-						color = 3'b100;
-					end
-				filter = 2'b11;
-            end
-		endcase
-    end
+				
+						GreenFreq = Freq;
+						filter = 2'b10;
+						Freq = 0;
+				
+      		end
+				2'b10: //clear
+					begin
+			  		if(RedFreq < BlueFreq && RedFreq < GreenFreq && RedFreq < 24)
+							begin
+				  			color = 3'b001;
+							end
+			  		else if (BlueFreq < RedFreq && BlueFreq < GreenFreq && BlueFreq < 21)
+			  			begin
+								color = 3'b010;
+							end
+			  		else if (GreenFreq < RedFreq && GreenFreq < BlueFreq && GreenFreq < 19)
+			  			begin
+								color = 3'b100;
+							end
+			  		filter = 2'b00;
+			   	
+				end
+			endcase
+  	end
 
 endmodule
