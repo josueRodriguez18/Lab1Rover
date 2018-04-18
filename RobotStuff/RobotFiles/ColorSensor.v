cdd1b24483ca29cd99debee830cd5dda9942950f
Dong Hyuk Shin <35859492+Anthony-Shin@users.noreply.github.com>
2018-04-18 16:29:33 -0500

    Maybe the Final

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,6 +1,6 @@
 module ColorSensor(output reg [1:0] scale, output reg [1:0]filter, output reg [2:0] color, input sensorFreq, input clk);
 reg [31:0] tempFreq = 0;
-reg [31:0] RedFreq; //reg [31:0] BlueFreq; reg [31:0] GreenFreq; 
+reg [31:0] RedFreq; reg [31:0] BlueFreq; reg [31:0] GreenFreq; 
 reg [31:0] count = 0; 
 reg gate = 0;
 always@(posedge clk)
@@ -27,33 +27,63 @@ always@(posedge clk)
 							end
 						
 					end
-				// 2'b01: //BLUE
-				// 	begin
-				// 		BlueFreq = Freq;
-				// 		filter = 2'b11;
-				// 	end
-				// 2'b11: //GREEN
-				// 	begin
-				// 		GreenFreq = Freq;
-				// 		filter = 2'b10;
-      		    //     end
-				// 2'b10: //clear
-				// 	begin
-			  	// 	if(RedFreq < BlueFreq && RedFreq < GreenFreq && RedFreq < 24)
-				// 		begin
-				//   			color = 3'b001;
-				// 		end
-			  	// 	else if (BlueFreq < RedFreq && BlueFreq < GreenFreq && BlueFreq < 21)
-			  	// 		begin
-				// 			color = 3'b010;
-				// 		end
-			  	// 	else if (GreenFreq < RedFreq && GreenFreq < BlueFreq && GreenFreq < 19)
-			  	// 		begin
-				// 			color = 3'b100;
-				// 		end
-			  	// 	filter = 2'b00;
+				2'b01: //BLUE
+					begin
+						if(count < 100000)
+							begin
+								count = count + 1;
+								gate = 1;
+							end
+						if(count == 100000)
+							begin
+								gate = 0;
+								count = count + 1;
+								BlueFreq = tempFreq;
+							end
+						else
+							begin
+								count = 0;
+								tempFreq = 0;
+								filter = 2'b01;
+							end
+					end
+				2'b11: //GREEN
+					begin
+						if(count < 100000)
+							begin
+								count = count + 1;
+								gate = 1;
+							end
+						if(count == 100000)
+							begin
+								gate = 0;
+								count = count + 1;
+								GreenFreq = tempFreq;
+							end
+						else
+							begin
+								count = 0;
+								tempFreq = 0;
+								filter = 2'b01;
+							end
+      		  end
+				2'b10: //clear
+					begin
+			  		if(RedFreq < BlueFreq && RedFreq < GreenFreq && RedFreq < 24)
+						begin
+				  			color = 3'b001;
+						end
+			  		else if (BlueFreq < RedFreq && BlueFreq < GreenFreq && BlueFreq < 21)
+			  			begin
+							color = 3'b010;
+						end
+			  		else if (GreenFreq < RedFreq && GreenFreq < BlueFreq && GreenFreq < 19)
+			  			begin
+							color = 3'b100;
+						end
+			  		filter = 2'b00;
 			   	
-				// end
+				end
 			endcase
   	end
 
