2329201de6478c2d52e13bf0acbb48cf7a31cda6
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-04-18 14:55:31 -0500

    made more readable

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -8,39 +8,31 @@ always@(Freq)
 					begin				
 						RedFreq = Freq;
 						filter = 2'b01;
-						
-
 					end
 				2'b01: //BLUE
 					begin
-				
 						BlueFreq = Freq;
 						filter = 2'b11;
-						
-				
 					end
 				2'b11: //GREEN
 					begin
-				
 						GreenFreq = Freq;
 						filter = 2'b10;
-						
-				
       		        end
 				2'b10: //clear
 					begin
 			  		if(RedFreq < BlueFreq && RedFreq < GreenFreq && RedFreq < 24)
-							begin
+						begin
 				  			color = 3'b001;
-							end
+						end
 			  		else if (BlueFreq < RedFreq && BlueFreq < GreenFreq && BlueFreq < 21)
 			  			begin
-								color = 3'b010;
-							end
+							color = 3'b010;
+						end
 			  		else if (GreenFreq < RedFreq && GreenFreq < BlueFreq && GreenFreq < 19)
 			  			begin
-								color = 3'b100;
-							end
+							color = 3'b100;
+						end
 			  		filter = 2'b00;
 			   	
 				end
