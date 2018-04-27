1dc067e993da242366dc8725a46e3f1eb6851a05
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-04-27 17:06:04 -0500

    fixed formatting

--- RobotStuff/RobotFiles/BasicMotor.v
+++ RobotStuff/RobotFiles/BasicMotor.v
@@ -4,33 +4,33 @@ reg [3:0] last; reg [1:0]proxim_last; reg at_Proxim; reg [2:0]induct_last;
     always @(proxim || induct) //any time induct changes
         begin
              case (proxim) //proxim case
-              1'b0: //low
-                casex(induct) 
-                    3'b001: //left and center detecting
-                        begin //have to turn left
-                           if(proxim_last)
-                               begin
-                                    motorIn <= 4'b0110;
-                                    proxim_last <= 0;
-                               end
-                           else
-                               begin
-                                    motorIn <= 4'b1010; //left motor backward right motor forward
-                                    last = 4'b1010; //save last
-                                end
-                        end
+                1'b0: //low
+                    case(induct) 
+                        3'b001: //left and center detecting
+                            begin //have to turn left
+                                if(proxim_last)
+                                    begin
+                                        motorIn <= 4'b0110;
+                                        proxim_last <= 0;
+                                    end
+                                else
+                                    begin
+                                        motorIn <= 4'b1010; //left motor backward right motor forward
+                                        last = 4'b1010; //save last
+                                    end
+                            end
                     3'b011:
-                         begin
+                        begin
                             if(at_Proxim)
                                 begin
-                                  motorIn <= last;
+                                    motorIn <= last;
                                 end
                             else
                                 begin
                                     motorIn <= 4'b1010;
                                     last <= 4'b1010;
                                 end
-                         end
+                        end
                         
                     3'b110:
                         begin
@@ -47,19 +47,19 @@ reg [3:0] last; reg [1:0]proxim_last; reg at_Proxim; reg [2:0]induct_last;
                     3'b100:
                         begin
                             if(at_Proxim)
-                               begin
-                                  motorIn <= last;
+                                begin
+                                    motorIn <= last;
                                 end
                              else
                                begin
-                                  motorIn <= 4'b0101;
-                                  last = motorIn;
+                                    motorIn <= 4'b0101;
+                                    last = motorIn;
                                end
                         end
                     3'b000:
                         begin
-                          motorIn <= 4'b0101;
-                          last <= 4'b0101;
+                            motorIn <= 4'b0101;
+                            last <= 4'b0101;
                         end
                     3'b101:
                         begin
