9d126dc93b7bd6cf87d5d6dc5cf1fe428e2bd014
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-04-27 17:07:28 -0500

    changed formatting

--- RobotStuff/RobotFiles/Top.v
+++ RobotStuff/RobotFiles/Top.v
@@ -5,10 +5,6 @@ module Top(output [6:0]JA, input [3:0]JB, input clk);
             assign echo = JB[3];
             assign induct[2:0] = JB[2:0];
             assign MotorIn = JA[3:0];
-             Motor motorTest(induct, proxim, MotorIn);
-             ProximitySensor prox(trigger, echo, clk, proxim);
-            
-            
-            
-            
+            Motor motorTest(induct, proxim, MotorIn);
+            ProximitySensor prox(trigger, echo, clk, proxim);       
 endmodule
