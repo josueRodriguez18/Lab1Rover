00e6e6a91e6f4e96aabcf81f5ce2ad93b0bf0098
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-05-04 22:46:42 -0500

    modified color sensor code, hasn't been tested

--- RobotStuff/RobotFiles/Top.v
+++ RobotStuff/RobotFiles/Top.v
@@ -1,10 +1,13 @@
 module Top(output [6:0]JA, input [3:0]JB, input clk); 
-            wire proxim; wire echo; wire trigger;
-            wire [2:0]induct; wire [3:0]MotorIn;
-            assign trigger = JA[4];
-            assign echo = JB[3];
-            assign induct[2:0] = JB[2:0];
-            assign MotorIn = JA[3:0];
-            Motor motorTest(induct, proxim, MotorIn);
-            ProximitySensor prox(trigger, echo, clk, proxim);    
+//            wire proxim; wire echo; wire trigger;
+//            wire [2:0]induct; wire [3:0]MotorIn;
+//            assign trigger = JA[4];
+//            assign echo = JB[3];
+//            assign induct[2:0] = JB[2:0];
+//            assign MotorIn = JA[3:0];
+//            Motor motorTest(induct, proxim, MotorIn);
+//            ProximitySensor prox(trigger, echo, clk, proxim); 
+              reg color; wire [1:0]sensorFreq;
+              assign sensorFreq = JB[1:0];
+              ColorSensor c1(color, clk, sensorFreq); 
 endmodule
