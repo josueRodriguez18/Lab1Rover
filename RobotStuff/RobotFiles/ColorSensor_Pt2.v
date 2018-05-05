afaa8991e4f7c684c68bf98d8ad0343e1f399d04
Dong Hyuk Shin <35859492+Anthony-Shin@users.noreply.github.com>
2018-05-05 04:30:55 -0500

    added enables

--- RobotStuff/RobotFiles/ColorSensor_Pt2.v
+++ RobotStuff/RobotFiles/ColorSensor_Pt2.v
@@ -1,8 +1,10 @@
-module ColorSensor(output reg color, output reg [1:0]filter, input clk, input Freq);
+module ColorSensor_Pt2(output reg color, output reg [1:0]filter, output reg enable,input clk, input Freq);
 reg [31:0] greenTemp; 
 reg [31:0] GreenFreq; reg [31:0] ClearFreq; 
 reg [31:0] count; 
 reg [31:0] percent;
+reg [31:0] Freq;
+reg enable;
 
 always@(Freq)
 	
@@ -10,34 +12,53 @@ always@(Freq)
 	
 	2'b10: //clear
 		begin
+				enable = 0;
 				ClearFreq <= Freq;
 				filter = filter + 1;
 		end
 	
 	2'b11: // green
     	begin	
+				enable = 0;
 				GreenFreq <= Freq;			
 				Greentemp <= GreenFreq * 100;
-				filter = filter -1;
+				enable = 1;
+				filter <= 2'b00;
 		end
-
+	2'b00: // calc
+		begin
+			if(Greentemp >= ClearFreq)
+				begin
+				  Greentemp = Greentemp - ClearFreq;
+			 	  count = count + 1;
+				end
+			else
+				begin
+					percent <= count;
+			  		if(57<= percent <=80)
+			  			begin
+							color =1;
+						end
+				end
+		end
+		enable = 0;
 	endcase
 
-always@(posedge clk)
-	begin
-		if(Greentemp >= ClearFreq)
-			begin
-			  Greentemp = Greentemnp - ClearFreq;
-			  count = count + 1;
-			end
-		else
-			begin
-			  percent <= count;
-			  if(57<= percent <=80)
-			  	begin
-					color =1;
-				end
-			end
-	end
+// always@(posedge clk)
+// 	begin
+// 		if(Greentemp >= ClearFreq)
+// 			begin
+// 			  Greentemp = Greentemp - ClearFreq;
+// 			  count = count + 1;
+// 			end
+// 		else
+// 			begin
+// 			  percent <= count;
+// 			  if(57<= percent <=80)
+// 			  	begin
+// 					color =1;
+// 				end
+// 			end
+// 	end
 
 endmodule
