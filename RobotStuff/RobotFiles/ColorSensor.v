e0283421a6782dc9febd1bde0a5a67ba4d20a4b9
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-05-04 22:58:26 -0500

    bitstream was succesfull

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,81 +1,66 @@
-module ColorSensor(output reg color, output reg [1:0] scale, output reg [1:0] scale2, input sensorFreq, input sensorFreq2, input clk);
-reg [31:0] tempFreq = 0;
-reg [31:0] tempFreq2 = 0;
-reg [31:0] GreenFreq; reg [31:0] clearFreq; 
-reg [31:0] tempGreenFreq; reg[31:0] percent;
-reg [31:0] count = 0;
-reg [31:0] count2 = 0;
-reg[31:0] count3 = 0;
-reg [1:0] scale = 2'b11;
-reg [1:0] scale2 = 2'b11;
-
-reg last = 0;
-reg last2 = 0;
+module ColorSensor(output reg color, input clk, input [1:0]sensorFreq);
+reg [31:0] greenTemp; reg [31:0] clearTemp; reg[31:0] temp;
+reg [31:0] GreenFreq; reg [31:0] ClearFreq; 
+reg [31:0] count; 
+reg gate; reg [31:0] percent;
 always@(posedge clk)
-    begin
-        last<= sensorFreq;
-		
-		if(count <= 100000)
+    begin				
+				if(count < 100000)
+					begin
+						count <= count + 1;
+						gate <= 1;
+					end
+				else if(count == 100000)
+					begin
+						GreenFreq <= greenTemp;
+						ClearFreq <= clearTemp;
+						temp <= GreenFreq * 100;
+						count <= count + 1;
+					end
+				else
+					begin
+						gate <= 0;
+						if(ClearFreq <= temp)
+							begin
+								temp <= temp - ClearFreq;
+								count <= count +1;
+							end
+						else
+							begin
+								percent <= count;
+								if(57 <= percent && percent <= 80)
+									begin
+										color <= 1;
+									end
+								count <= 0;
+							end
+
+					end
+				
+  	end
+
+always@(posedge (sensorFreq[1]))
+	begin
+		if(gate == 1)
 			begin
-				count <= count + 1;
-						
-				if(~last & sensorFreq)
-				    begin
-				        tempFreq <= tempFreq + 1;
-				    end
+				greenTemp <= greenTemp +1;
 			end
-			
 		else
 			begin
-				GreenFreq <= tempFreq;
-				tempFreq <= 0;
-				count <= 0;
-				
+				greenTemp <= 0;
 			end
-      	
-		//clear
-		
-		if(count2 <= 100000)
+	end
+always@(posedge(sensorFreq[0]))
+	begin
+		if(gate == 1)
 			begin
-				count2 <= count2 + 1;
-				
-				if(~last2 & sensorFreq2)
-				    begin
-				        tempFreq2 <= tempFreq2 + 1;
-				    end
-				end
-				
-			else
-				begin
-					clearFreq <= tempFreq2;
-					tempFreq2 <= 0;
-					count2 <= 0;
-					
-				end
+				clearTemp <= clearTemp +1;
 			end
-		
-  	
- always@(GreenFreq && clearFreq)
- 	begin
- 		tempGreenFreq <= GreenFreq * 100;
- 		if(clearFreq <= tempGreenFreq)
- 			begin
- 				tempGreenFreq <= clearFreq;
- 				count3 <= count3 +1;
- 			end
- 		else
- 			begin
- 			  percent <= count3;
- 			  count3 <= 0;
- 			  if(57 < percent < 80)
-                         begin
-                             color = 1;
-                         end
- 			end
-		
-		
-		
- 	end
+		else
+			begin
+				clearTemp <= 0;
+			end
+	end
 
 
 endmodule
