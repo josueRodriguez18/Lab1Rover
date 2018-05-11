cc4f8053ff13cadf5fb8baa2b5630a378501f822
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-05-10 20:05:03 -0500

    finalized

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,70 +1,32 @@
-module ColorSensor(output reg color, input clk, input [1:0]sensorFreq);
+module ColorSensor(output reg color, input clk, output reg enable);
 reg [31:0] greenTemp; reg [31:0] clearTemp; reg[31:0] temp;
 reg [31:0] GreenFreq; reg [31:0] ClearFreq; 
 reg [31:0] count; 
 reg gate; reg [31:0] percent;
 always@(posedge clk)
-    begin				
-				if(count < 100000)
+	begin
+		if(count <= 100000)
+			begin
+				if(~last &&freq)
 					begin
-						count <= count + 1;
-						gate <= 1;
+						tempFreq <= tempFreq + 1;
 					end
-				else if(count == 100000)
+				count <= count + 1;
+			end
+		else
+			begin
+				count <= 0;
+				GreenFreq <= tempFreq;
+				if(GreenFreq < 58 && GreenFreq > 52)
 					begin
-						GreenFreq <= greenTemp;
-						ClearFreq <= clearTemp;
-						temp <= GreenFreq * 100;
-						count <= count + 1;
-					end
+						color <= 1;
+					end 
+
 				else
 					begin
-						gate <= 0;
-						if(ClearFreq <= temp)
-							begin
-								temp <= temp - ClearFreq;
-								count <= count +1;
-							end
-						else
-							begin
-								percent <= count;
-								if(57 <= percent && percent <= 80)
-									begin
-										color <= 1;
-									end
-								else
-								    begin
-								        color <= 0;
-								    end
-								count <= 0;
-							end
-
+						color <= 0;
 					end
-				
-  	end
-
-always@(posedge (sensorFreq[1]))
-	begin
-		if(gate == 1)
-			begin
-				greenTemp <= greenTemp +1;
 			end
-		else
-			begin
-				greenTemp <= 0;
-			end
+	last <= freq;
 	end
-always@(posedge(sensorFreq[0]))
-	begin
-		if(gate == 1)
-			begin
-				clearTemp <= clearTemp +1;
-			end
-		else
-			begin
-				clearTemp <= 0;
-			end
-	end
-
-
 endmodule
--- RobotStuff/RobotFiles/Top.v
+++ RobotStuff/RobotFiles/Top.v
@@ -1,7 +1,9 @@
-module Top(input [7:5]JB, input clk, output [3:2]JC, input [1:0]sw, output [6:0]seg, output [3:0]an);
-			reg [15:0]display_number = 2345;
+module Top(input [7:5]JB, input clk, output [0:0]led);
+		reg [15:0] displayed_number;
 			wire [3:0]anode_activate;
 			wire [6:0]led_out;
+			reg [26:0] one_second_counter; // counter for generating 1 second clock enable
+            wire one_second_enable;
 //            wire proxim; wire echo; wire trigger;
 //            wire [2:0]induct; wire [3:0]MotorIn;
 //            assign trigger = JA[4];
@@ -10,11 +12,26 @@ module Top(input [7:5]JB, input clk, output [3:2]JC, input [1:0]sw, output [6:0]
 //            assign MotorIn = JA[3:0];
 //            Motor motorTest(induct, proxim, MotorIn);
 //            ProximitySensor prox(trigger, echo, clk, proxim);
-                //reg [31:0] freqValue; wire sensorFreq;
-                //assign sensorFreq = JB[7];
-                //FreqCounter f1(sw[0], sensorFreq, input clk, freqValue);
-			assign an[3:0] = anode_activate[3:0];
-			assign seg[6:0] = led_out[3:0];
-              SegDisplay s1(display_number, anode_activate, led_out, clk);
-
+                reg [31:0] freqValue; wire sensorFreq; wire color;
+                assign color = led[0];
+                assign sensorFreq = JB[7]; 
+                ColorSensor c1(color, sensorFreq, clk);
+			
+//			assign an[3:0] = anode_activate[3:0];
+//			assign seg[6:0] = led_out[6:0];
+//              SegDisplay s1(displayed_number, anode_activate, led_out, clk);
+            
+//                always @(posedge clk)
+//              begin
+//                      if(one_second_counter>=99999999) 
+//                           one_second_counter <= 0;
+//                      else
+//                          one_second_counter <= one_second_counter + 1;
+//              end 
+//              assign one_second_enable = (one_second_counter==99999999)?1:0;
+//              always @(posedge clk)
+//              begin
+//              if(one_second_enable==1)
+//                      displayed_number <= displayed_number + 1;
+//              end
 endmodule
