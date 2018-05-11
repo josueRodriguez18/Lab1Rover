c0b35a6b46973f6ad533e1a1406c946dfb15d495
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-05-10 20:08:06 -0500

    finallly finalized

--- RobotStuff/RobotFiles/ColorSensor.v
+++ RobotStuff/RobotFiles/ColorSensor.v
@@ -1,32 +1,33 @@
-module ColorSensor(output reg color, input clk, output reg enable);
-reg [31:0] greenTemp; reg [31:0] clearTemp; reg[31:0] temp;
-reg [31:0] GreenFreq; reg [31:0] ClearFreq; 
+module ColorSensor(output reg color, input clk, input freq);
+reg [31:0] greenTemp; reg[31:0] tempFreq; reg last = 0;
+reg [31:0] GreenFreq; 
 reg [31:0] count; 
-reg gate; reg [31:0] percent;
 always@(posedge clk)
 	begin
-		if(count <= 100000)
+		if(count <= 200000)
 			begin
 				if(~last &&freq)
 					begin
-						tempFreq <= tempFreq + 1;
+						tempFreq <= tempFreq + 1;	
 					end
 				count <= count + 1;
+				last <= freq;
 			end
 		else
 			begin
 				count <= 0;
 				GreenFreq <= tempFreq;
-				if(GreenFreq < 58 && GreenFreq > 52)
+				if(GreenFreq > 0)
 					begin
 						color <= 1;
+						tempFreq <= 0;
 					end 
 
 				else
 					begin
 						color <= 0;
+						tempFreq <= 0;
 					end
 			end
-	last <= freq;
 	end
 endmodule
